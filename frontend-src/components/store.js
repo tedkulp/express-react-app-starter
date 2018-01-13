27ccc1205a0789f021f5471d88b59a87e3965fea
Ted Kulp <ted@tedkulp.com>
2018-01-12 19:52:17 -0500

    Add the redux-saga setup

--- frontend-src/components/store.js
+++ frontend-src/components/store.js
@@ -1,5 +1,6 @@
 import { createStore as _createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
 import reducer from './reducer';
 import { routerMiddleware } from 'react-router-redux';
 import logger from 'redux-logger';
@@ -8,7 +9,7 @@ import createHistory from 'history/createBrowserHistory';
 export function createStore(history, client, data) {
   const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
   const historyMiddleware = routerMiddleware(history);
-  const middleware = [thunk, historyMiddleware, logger];
+  const middleware = [thunk, historyMiddleware, logger, createSagaMiddleware()];
   const finalCreateStore = composeEnhancers(applyMiddleware(...middleware))(_createStore);
   const store = finalCreateStore(reducer, data);
 
