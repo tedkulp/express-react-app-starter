7602f9de106b9026ade7cd904f51496ff0d58738
Ted Kulp <ted@tedkulp.com>
2018-01-13 09:37:30 -0500

    Use reactstrap next for now

--- setup.sh
+++ setup.sh
@@ -11,7 +11,7 @@ cd frontend
 
 yarn add axios bluebird bootstrap final-form font-awesome
 yarn add lodash moment popper.js prop-types reactdom react-final-form
-yarn add react-redux react-router-dom react-router-redux@next reactstrap
+yarn add react-redux react-router-dom react-router-redux@next reactstrap@next
 yarn add redux redux-logger redux-thunk redux-saga serve
 
 cd src
