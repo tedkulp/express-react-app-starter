de503a61422fdd1d4d3ad7073be92523a7b40fea
Ted Kulp <ted@tedkulp.com>
2018-01-14 11:36:30 -0500

    Added handling for backend running independently

---
+++ backend/wait-for-services.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+DOCKER_HOST=$(docker-machine ip)
+HOST=${1:-$DOCKER_HOST}
+
+set -eu pipefail
+
+echo "Attempting to connect to mongodb"
+until $(nc -zv $HOST 27017); do
+    printf '.'
+    sleep 5
+done
+echo "Attempting to connect to redis"
+until $(nc -zv $HOST 6379); do
+    printf '.'
+    sleep 5
+done
+echo "Was able to connect to all"
+
+exit 0
