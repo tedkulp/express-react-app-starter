5c25159e33c7626bd1985859b2901704648334c0
Ted Kulp <ted@tedkulp.com>
2019-07-29 08:02:19 -0400

    Use env variables for mongo and redis hosts in wait script

--- backend/wait-for-services.sh
+++ backend/wait-for-services.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 
-# DOCKER_HOST=$(docker-machine ip)
-# DOCKER_HOST="127.0.0.1"
-# MONGO_HOST=${1:-mongo}
-# REDIS_HOST=${2:-redis}
+MONGO_HOST=${DOCKER_MONGODB_HOST:-mongo}
+REDIS_HOST=${DOCKER_REDIS_HOST:-redis}
 
-# echo "$1 $2"
+echo "--> $MONGO_HOST $REDIS_HOST <--"
 
 set -eu pipefail
 
 echo "Attempting to connect to mongodb"
-until $(nc -zv mongo 27017); do
+until $(nc -zv $MONGO_HOST 27017); do
     printf '.'
     sleep 5
 done
 echo "Attempting to connect to redis"
-until $(nc -zv redis 6379); do
+until $(nc -zv $REDIS_HOST 6379); do
     printf '.'
     sleep 5
 done
